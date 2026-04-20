5a0bd9b463d7ab47b0aefb12306a052c96110062
Xerx81 <ryuvraj832@gmail.com>
2026-04-20 11:49:08 +0530

    removed old backend and frontend to start from scratch

--- backend/restaurant/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from . import views
-
-
-urlpatterns = [
-    path('items/', views.MenuItemView.as_view(), name="items"),
-    path('items/<int:pk>', views.SingleMenuItemView.as_view()),
-]
